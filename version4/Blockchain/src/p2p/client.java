a3203b4c36706b9cc5e19c7ee558bd2fd043396e
Mari <mari-191938@hotmail.com>
2020-12-01 23:12:42 -0300

    comparando arquivo

--- version4/Blockchain/src/p2p/client.java
+++ version4/Blockchain/src/p2p/client.java
@@ -10,7 +10,7 @@ public class client {
 			Socket socket = new Socket("192.168.1.173", 6666);
 			dataInputStream = new DataInputStream(socket.getInputStream());
 			int bytes = 0;
-			FileOutputStream fileOutputStream = new FileOutputStream("originalCodes.txt");
+			FileOutputStream fileOutputStream = new FileOutputStream("copia.txt");
 			System.out.println("Aguardando envio...");
 			long size = dataInputStream.readLong(); // read file size
 			byte[] buffer = new byte[4 * 1024];
@@ -18,11 +18,37 @@ public class client {
 				fileOutputStream.write(buffer, 0, bytes);
 				size -= bytes; // read upto file size
 			}
-			System.out.println("Arquivo recebido.");
+			compareFile();
 			fileOutputStream.close();
 			dataInputStream.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void compareFile() {
+		try {
+			File file = new File("copia.txt");
+			File file2 = new File("originalCodes.txt");
+			BufferedReader bf = new BufferedReader(new FileReader(file));
+			BufferedReader br = new BufferedReader(new FileReader(file2));
+			String line1 = bf.readLine();
+			String line2 = br.readLine();
+
+			while (line1 != null && line2 != null) {
+				if (!line1.contains(line2)) {
+					System.out.println("Arquivo adulterado, transferencia encerrada.");
+					file.delete();
+				}
+			
+				line1 = bf.readLine();
+				line2 = br.readLine();
+			}
+			file2.renameTo(new File("originalCodes.txt"));
+			System.out.println("Arquivo recebido.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
